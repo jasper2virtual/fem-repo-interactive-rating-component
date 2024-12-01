73d8ccfbe3d04d1d4b237f8b3fe6bbfca6aaee0a
Virtual Jasper <jasper2virtual@hotmail.com>
2024-12-01 23:52:09 +0800

    in progress, i am very lazy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
 import themes from 'daisyui/src/theming/themes'
+
+const appColors = {
+  'dark-blue':'#262E38',
+  'light-grey':'#969FAD',
+  'orange':'#FC7614',
+  'pure-white':'#FFFFFF',
+  'very-dark-blue':'#131518',
+}
+
 export default {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [daisyui],
   daisyui: {
